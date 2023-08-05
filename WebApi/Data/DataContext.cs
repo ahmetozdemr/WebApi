e9c84c202d5ab3fe43912ef06521b4e180b5493b
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-08-05 23:16:59 +0300

    Add project files.

---
+++ WebApi/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
+
